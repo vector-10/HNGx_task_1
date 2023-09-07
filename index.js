a2f31d8922ef8c1a0002ba6d35604955bf1c982b
Vector-9 <duzieblaise10@gmail.com>
2023-09-07 22:29:43 +0100

    api complete

--- index.js
+++ index.js
@@ -5,5 +5,46 @@ const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 
+app.get("/api", (req, res) => {
+  const { track, slack_name } = req.query;
+
+  if (!track || !slack_name) {
+    return res.status(400).json({
+      error: "track and slack_name are required to make this request",
+    });
+  }
+  // get current day of the week in string
+  const currentDay = new Date().toLocaleString("default", {
+    weekday: "long",
+  });
+  // get current utc time
+  const currentUtc = new Date().toUTCString();
+
+  //validation for ( -2/+2 ) time zone
+  const utcDifferenceHours = new Date().getTimezoneOffset() / 60;
+  if (utcDifferenceHours < -2 || utcDifferenceHours > 2) {
+    return res
+      .status(400)
+      .json({ error: "UTC time is not within +/-2 hours from UTC" });
+  }
+
+  //url of the main file running the code
+  const mainFileUrl =
+    "https://github.com/vector-10/HNGx_task_1/blob/main/index.js";
+
+  // url of the repository
+  const repoUrl = "https://github.com/vector-10/HNGx_task_1";
+
+  res.status(200).json({
+    slack_name: slack_name,
+    currentDay: currentDay,
+    currentUtcTime: currentUtc,
+    track: track,
+    githubFileUrl: mainFileUrl,
+    githubRepoUrl: repoUrl,
+    status_code: 200,
+  });
+});
+
 const PORT = 2000 || process.env.PORT;
 app.listen(PORT, () => console.log(`server is listening on PORT ${PORT}`));
